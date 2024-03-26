2a8c9e12b93864cc51db2b8055f35e14668c4970
20221TIIMI0300 <marcellagaurink@gmail.com>
2024-03-26 09:13:26 -0300

    comeco dos comentarios

--- app/src/main/java/gaurink/mar/primeiraapp/MainActivity.java
+++ app/src/main/java/gaurink/mar/primeiraapp/MainActivity.java
@@ -7,18 +7,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
+/*A classe MainActivity e a tela principal da aplicaçao*/
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //onCreate e o metodo chamado toda vez que a tela esta sendo criada//
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /* setContentView construi os elementos de interface */
 
-        Button btnEnviar = findViewById(R.id.btnEnviar);
-        btnEnviar.setOnClickListener(new View.OnClickListener() {
+        Button btnEnviar = findViewById(R.id.btnEnviar); //obtem o botao enviar//
+        btnEnviar.setOnClickListener(new View.OnClickListener() { //onClick define o que acontece quando clica no botao//
             @Override
-            public void onClick(View v) {
+            public void onClick(View v) { //executado quando o botao e clicado//
                 EditText etDigiteAqui = findViewById(R.id.etDigiteAqui);
                 String textoDigitado = etDigiteAqui.getText().toString();
                 Intent i = new Intent(MainActivity.this,NextActivity.class);
